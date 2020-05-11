d55e7a0d48792f1a6eecc551b857b1ca3d8c6d21
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-11 20:33:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 
 end
@@ -13,4 +13,4 @@ end
 lassie = Dog.new
 lassie.name = "Lassie"
 
-lassie.name
+puts lassie.name
